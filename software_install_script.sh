818c2d67152337438bab46639287854f3e841022
Damian Pieczykolan / Art3usz <Damian.Pieczykolan@10g.pl>
2020-09-02 08:25:37 +0200

    repar loop in script

--- software_install_script.sh
+++ software_install_script.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-declare -a LIST=("vim" "autoconf" "automake" "cmake" "gcc" "gdb" "git" "kile" "kile-l10n" "libtool" "make" "mc" "mesa-utils" "mesa-utils-extra" "qtbase5-dev" "qtcreator" "subversion" "texlive" "texlive-full" "virtualbox" "ghc" "gprolog" "ruby" "bison" "flex" "gfortran" "python-vispy" "python-tk" "python3" "python3-matplotlib" "python3-numpy" "python3-pip" "python3-tk" "python3-venv" "python3-virtualenv" "python3-vispy" "virtualenv" "virtualbox" "virtualbox—ext–pack")
+LIST=("vim" "autoconf" "automake" "aptitude" "cmake" "gcc" "gdb" "git" "kile" "kile-l10n" "libtool" "make" "mc" "mesa-utils" "mesa-utils-extra" "qtbase5-dev" "qtcreator" "subversion" "texlive" "texlive-full" "virtualbox" "ghc" "gprolog" "ruby" "bison" "flex" "gfortran" "python3-vispy" "python-tk" "python3" "python3-matplotlib" "python3-numpy"  "python2" "python-tk" "python3-tk" "python3-venv" "python3-virtualenv" "python3-vispy" "virtualenv")
 
 if [[ $EUID -ne 0 ]];
 then
   echo "Proszę uruchomić skrypt jako root."
   exit
 else
+    clear
     apt update
-    for s in $LIST
+    for index in ${!LIST[*]}
     do
-        echo "Installing $s\n"
-        apt install -y $s; 
+    	echo ''
+    	echo ''
+    	echo "Installing ${LIST[$index]}"
+    	echo ''
+	apt install -y ${LIST[$index]}; 
     done
     apt full-upgrade -y
+    apt autoclean 
 fi
+history -c
