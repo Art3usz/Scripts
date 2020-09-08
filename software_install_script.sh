a647f4af4ff9dd14e5cc44fd9bf6d85c570fa7e1
Damian Pieczykolan / Art3usz <Damian.Pieczykolan@10g.pl>
2020-09-08 11:29:27 +0200

    small update

--- software_install_script.sh
+++ software_install_script.sh
@@ -7,27 +7,30 @@ function pisz {
     echo ""
 }
 
-LIST=("kubuntu-desktop" "vim" "autoconf" "automake" "aptitude" "cmake" "gcc" "gdb" "git" "kile" "kile-l10n" "libtool" "make" "mc" "mesa-utils" "mesa-utils-extra" "qtbase5-dev" "qtcreator" "subversion" "texlive" "texlive-full" "virtualbox" "ghc" "gprolog" "ruby" "bison" "flex" "gfortran" "python3-vispy" "python-tk" "python3" "python3-matplotlib" "python3-numpy"  "python2" "python-tk" "python3-tk" "python3-venv" "python3-virtualenv" "python3-vispy" "virtualenv" "g++" "qtbase5-dev-tools" "geany-common" "gimp" "default-jre" "default-jdk")
-
+ListOfPackages=("kubuntu-desktop" "vim" "autoconf" "automake" "aptitude" "cmake" "gcc" "gdb" "git" "kile" "kile-l10n" "libtool" "make" "mc" "mesa-utils" "mesa-utils-extra" "qtbase5-dev" "qtcreator" "subversion" "texlive" "texlive-full" "virtualbox" "ghc" "gprolog" "ruby" "bison" "flex" "gfortran" "python3-vispy" "python-tk" "python3" "python3-matplotlib" "python3-numpy"  "python2" "python-tk" "python3-tk" "python3-venv" "python3-virtualenv" "python3-vispy" "virtualenv" "g++" "qtbase5-dev-tools" "geany-common" "gimp" "default-jre" "default-jdk")
+ListOfRepo=("ppa:kubuntu-ppa/backports")
 if [[ $EUID -ne 0 ]];
 then
   echo "Proszę uruchomić skrypt jako root."
   exit
 else
     clear
-    pisz 'instalacja KDE'
-    add-apt-repository ppa:kubuntu-ppa/backports -y
+    for index in ${!ListOfRepo[*]}
+    do
+      pisz "Add repository ${ListOfRepo[$index]}"
+      add-apt-repository -y ${ListOfRepo[$index]};
+    done
     pisz 'Update repositories'
     apt update
     pisz 'Cleaning'
     apt autoremove -y
     pisz 'instalacja KDE'
     add-apt-repository ppa:kubuntu-ppa/backports
-    for index in ${!LIST[*]}
+    for index in ${!ListOfPackages[*]}
     do
     	
-    	pisz "Installing ${LIST[$index]}"
-    	apt install -y ${LIST[$index]}; 
+    	pisz "Installing ${ListOfPackages[$index]}"
+    	apt install -y ${ListOfPackages[$index]}; 
     done
     pisz 'Upgrade System'
     apt full-upgrade -y
