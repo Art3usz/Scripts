81542c2e77de139e6dab3ca1adca1a88ff61905c
Damian Pieczykolan / Art3usz <Damian.Pieczykolan@10g.pl>
2020-09-08 11:24:35 +0200

    update of list packeges and repositories

--- software_install_script.sh
+++ software_install_script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-LIST=("vim" "autoconf" "automake" "aptitude" "cmake" "gcc" "gdb" "git" "kile" "kile-l10n" "libtool" "make" "mc" "mesa-utils" "mesa-utils-extra" "qtbase5-dev" "qtcreator" "subversion" "texlive" "texlive-full" "virtualbox" "ghc" "gprolog" "ruby" "bison" "flex" "gfortran" "python3-vispy" "python-tk" "python3" "python3-matplotlib" "python3-numpy"  "python2" "python-tk" "python3-tk" "python3-venv" "python3-virtualenv" "python3-vispy" "virtualenv" "g++" "qtbase5-dev-tools" "geany-common" "gimp")
+
+function pisz {
+    echo ""
+    echo ""
+    echo $1
+    echo ""
+}
+
+LIST=("kubuntu-desktop" "vim" "autoconf" "automake" "aptitude" "cmake" "gcc" "gdb" "git" "kile" "kile-l10n" "libtool" "make" "mc" "mesa-utils" "mesa-utils-extra" "qtbase5-dev" "qtcreator" "subversion" "texlive" "texlive-full" "virtualbox" "ghc" "gprolog" "ruby" "bison" "flex" "gfortran" "python3-vispy" "python-tk" "python3" "python3-matplotlib" "python3-numpy"  "python2" "python-tk" "python3-tk" "python3-venv" "python3-virtualenv" "python3-vispy" "virtualenv" "g++" "qtbase5-dev-tools" "geany-common" "gimp" "default-jre" "default-jdk")
 
 if [[ $EUID -ne 0 ]];
 then
@@ -7,16 +15,27 @@ then
   exit
 else
     clear
+    pisz 'instalacja KDE'
+    add-apt-repository ppa:kubuntu-ppa/backports -y
+    pisz 'Update repositories'
     apt update
+    pisz 'Cleaning'
+    apt autoremove -y
+    pisz 'instalacja KDE'
+    add-apt-repository ppa:kubuntu-ppa/backports
     for index in ${!LIST[*]}
     do
-    	echo ''
-    	echo ''
-    	echo "Installing ${LIST[$index]}"
-    	echo ''
-	apt install -y ${LIST[$index]}; 
+    	
+    	pisz "Installing ${LIST[$index]}"
+    	apt install -y ${LIST[$index]}; 
     done
+    pisz 'Upgrade System'
     apt full-upgrade -y
-    apt autoclean 
+    
+    
+    pisz 'Cleaning'
+    apt autoremove -y
+    aptitude autoclean -y
+    apt autoclean -y
+    
 fi
-history -c
